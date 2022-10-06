b66ff5313caeccb17e80f90bea408a1722887074
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-10-06 16:47:56 +0200

    Added more comments

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -68,6 +68,7 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
+        // Effettuiamo la validazione per evitare che arrivino porcate sul DB
         $request->validate([
             'title' => 'required|string|unique:posts',
             'content' => 'nullable|string',
@@ -81,36 +82,57 @@ class PostController extends Controller
             'image.mimes' => 'L\'immagine deve avere estensione: jpg, png o jpeg',
         ]);
 
+        // Preleviamo tutti i dati dalla request
         $data = $request->all();
 
+        // Creiamo un nuovo post e facciamo il fill dei dati
         $new_post = new Post();
-
         $new_post->fill($data);
 
+        // Sluggiamo il titolo
         $new_post->slug = Str::slug($new_post->title,'-');
 
+        // Qui vengono riempiti alcune colonne del post a patto che siano state spuntate
+        // o meno nella creazione
         if(array_key_exists('is_published',$data)) $new_post->is_published = true;
         else $new_post->is_published = false;
 
         if(array_key_exists('category_id',$data)) $new_post->category_id = $data['category_id'];
 
         if(array_key_exists('image',$data)){
+            // # Storiamo l'immagine nella cartella storage-copia in public:
+            // # otteniamo un link assoluto che verrà salavto sul DB e che potrà
+            // # essere prelevato
             $image_link = Storage::put('posts_image',$data['image']);
             $new_post->image = $image_link;
         } 
 
+        // Facciamo in modo tale che il post sia legato all'utente
+        // loggato nella seguente sessione
         $new_post->user_id = Auth::id();
 
+        // Qui stiamo salvando il post sul database
         $new_post->save();
 
+        // @ Invio della mail
         if($new_post->is_published){
+
+            // Creiamo una nuova mail servendoci del costruttore del controller
             $mail = new PostPublicationMail($new_post);
+
+            // Qui definiamp chi è il ricevente della mail
             $receiver = Auth::user()->email;
+
+            // Servendoci della Facades: Mail inviamo l'email creata
             Mail::to($receiver)->send($mail);
         }
 
+        // Qui facciamo l'attach che consente di definire la relazione molti a molti
+        // tra posts e tags. Va fatto dopo perchè prima del save() il post non esiste sul
+        // DB, di conseguenza non esiste un id da passare nell'oggetto della classe ponte
         if(array_key_exists('tags',$data)) $new_post->tags()->attach($data['tags']);
 
+        // Effettuiamo il redirect
         return redirect()->route('admin.posts.index');
     }
 
@@ -149,6 +171,7 @@ class PostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
+        // Effettuiamo la validazione evitando qualche porcata nel DB
         $request->validate([
             'title' => ['required','string',Rule::unique('posts')->ignore($post->id)],
             'content' => 'nullable|string',
@@ -160,8 +183,10 @@ class PostController extends Controller
             'title.content' => 'Il content deve essere composta da caratteri',
         ]);
 
+        // preleviamo tutti i dati della request 
         $data = $request->all();
 
+        // @ Riempiamo alcune colonne dei post solo se checkati in fase di update
         if(array_key_exists('my_post',$data)) $post->user_id = Auth::id();
 
         if(array_key_exists('is_published',$data)) $post->is_published = true;
@@ -170,19 +195,27 @@ class PostController extends Controller
         if(array_key_exists('category_id',$data)) $post->category_id = $data['category_id'];
 
         if(array_key_exists('image',$data)){
+            // # Storiamo l'immagine nella cartella storage-copia in public:
+            // # otteniamo un link assoluto che verrà salavto sul DB e che potrà
+            // # essere prelevato
             if($post->image) Storage::delete($post->image);
             $image_link = Storage::put('posts_image',$data['image']);
             $post->image = $image_link;
         }
 
+        // @ Facciamo il sync dei tags: elimina quelli di prima e metti quelli checkati 
         if(array_key_exists('tags',$data)){
             $post->tags()->sync($data['tags']);
         }else{
+            // Nel caso in cui non arriva nulla vuol dire che sono stati tutti
+            // de-checkati e quindi col detach li leviamo tutti
             $post->tags()->detach();
         }
 
+        // Aggiorniamo il post nel DB
         $post->update($data);
 
+        // Effettuiamo il redirect
         return redirect()->route('admin.posts.show',compact('post'));
     }
 
@@ -194,10 +227,14 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
+        // Nel caso in cui dovesse esserci l'immagine va eliminata dalla cartella
+        // situata nello storage evitando di mantenere dati inutili
         if($post->image) Storage::delete($post->image);
 
+        // Cancellazione del post dal DB
         $post->delete();
         
+        // Effettuiamo il redirect
         return redirect()->route('admin.posts.index');
     }
 }
